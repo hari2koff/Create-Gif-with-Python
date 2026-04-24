2a0ec232c79da540484ab4625c89656a7f3b33ff
Hari <hari2koff@gmail.com>
2026-04-24 20:22:08 +0530

    images uploaded

--- create_gif.py
+++ create_gif.py
@@ -1,2 +1,10 @@
-import imageio
+# First Import our librabry  
+
+
+import imageio.v3 as iio     # here i am giving a nickname iio for easy usage        
+
+
+
+filenames = ['team-pic1.png', 'team-pic2.png']
+images = [ ]
 
