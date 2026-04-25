a54ca15708667d7a2e5605c1b9ad20cbe6b5677a
Hari <hari2koff@gmail.com>
2026-04-25 16:26:57 +0530

    Finished writing Code

--- create_gif.py
+++ create_gif.py
@@ -1,7 +1,40 @@
-# First Import our librabry  
+# First Import our library  
+import imageio.v3 as iio 
 
 
-import imageio.v3 as iio     # here i am giving a nickname iio for easy usage        
+
+# adding the file name with extension for iio.read method in lib to read the image
+
+file_names = ['team-pic1.png', 'team-pic2.png']   
+
+#Creating an empty list so the image read can be stored 
+ 
+images = []    
+
+# Creating a function that will create Gif 
+
+def create_gif():
+    # for loop to iterate over the list & , so it will iterate over all the elements 
+
+    for img in file_names:
+        images.append(iio.imread(img))  # .imread is a built in method which will read image and store th pixel data in memeory 
+
+    # Save as Gif by using the .imwrite method (which writes the stored pixel memory into file )    
+
+    iio.imwrite('Genatrated.gif', images, duration = 500, loop = 0)   
+
+
+create_gif()
+
+
+
+
+
+
+
+
+
+        
 
 
 
